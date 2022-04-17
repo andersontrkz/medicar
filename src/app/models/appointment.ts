f99f46d01cae0bdc6dc0d6a1b71bf986b6e8dfc1
andersontrkz <trkz.anderson@gmail.com>
2022-04-17 16:50:31 -0300

    chore(models): update models

--- src/app/models/appointment.ts
+++ src/app/models/appointment.ts
@@ -1,19 +1,9 @@
+import { Doctor } from "./doctor";
+
 export interface Appointment {
   id: number,
   dia: string,
   horario: string,
   data_agendamento: string,
   medico: Doctor
-}
-
-export interface Doctor {
-  id: number,
-  crm: number,
-  nome: string,
-  especialidade: Specialty
-}
-
-export interface Specialty {
-  id: number;
-  nome: string;
 }
--- src/app/models/schedule.ts
+++ src/app/models/schedule.ts
@@ -1,18 +1,8 @@
+import { Doctor } from "./doctor";
+
 export interface Schedule {
     id: number,
     medico: Doctor,
     dia: string,
     horarios: string[]
-}
-
-export interface Doctor {
-  id: number,
-  crm: number,
-  nome: string,
-  especialidade: Specialty
-}
-
-export interface Specialty {
-  id: number;
-  nome: string;
 }
