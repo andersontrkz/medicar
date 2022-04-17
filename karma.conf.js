12d72fbf6a9b9b679184363981c09bcc3b34a65f
andersontrkz <trkz.anderson@gmail.com>
2022-04-17 20:50:50 -0300

    refactor(project): to medicar name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/intmed'),
+      dir: require('path').join(__dirname, './coverage/medicar'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'intmed'`, () => {
+  it(`should have as title 'medicar'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('intmed');
+    expect(app.title).toEqual('medicar');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('intmed app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('medicar app is running!');
   });
 });
